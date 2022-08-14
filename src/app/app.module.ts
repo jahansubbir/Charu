4d38f18d5b7ef63e5cd0780779796df7687d3547
Mohammad Subbir Jahan Chowdhury <MSJ046@crb.apmoller.net>
2022-08-14 18:34:37 +0600

    basic front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu'
+import {MatIconModule} from '@angular/material/icon'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MdBootstrapModule } from './md-bootstrap-module/md-bootstrap.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,MatMenuModule,MatIconModule,MatSidenavModule,MdBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
